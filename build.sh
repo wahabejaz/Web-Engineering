0701ff2ab4c33c350fe765a6963b608145d83e60
Wahab Ejaz <wahabejaz.binary@gmail.com>
2026-05-11 19:26:40 +0500

    new

---
+++ build.sh
@@ -0,0 +1,7 @@
+﻿#!/usr/bin/env bash
+set -euo pipefail
+
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
