204aa6a41d9e881940f1dcfda745f279dacec06b
Wahab Ejaz <wahabejaz.binary@gmail.com>
2026-05-11 20:03:27 +0500

    Fix: Make STATICFILES_DIRS conditional to avoid missing directory warning, create static/ directory

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -137,7 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+] if (BASE_DIR / 'static').exists() else []
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
